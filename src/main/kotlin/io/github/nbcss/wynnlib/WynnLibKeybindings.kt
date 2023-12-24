0870a768733c7af6e48118fe14a4b52c688c01f0
Commander07 <45269106+Commander07@users.noreply.github.com>
2023-12-24 17:52:46 +0100

    feat: open content book keybind

--- src/main/kotlin/io/github/nbcss/wynnlib/WynnLibKeybindings.kt
+++ src/main/kotlin/io/github/nbcss/wynnlib/WynnLibKeybindings.kt
@@ -6,16 +6,22 @@ import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
 import net.minecraft.client.option.KeyBinding
 import net.minecraft.client.util.InputUtil
+import net.minecraft.screen.slot.SlotActionType
 import org.lwjgl.glfw.GLFW
 
 object WynnLibKeybindings {
     private val lockSlot = registerKey("wynnlib.key.slot_lock", GLFW.GLFW_KEY_L, true)
     private val openHandbook = registerKey("wynnlib.key.handbook", GLFW.GLFW_KEY_H)
     private val toggleAnalyze = registerKey("wynnlib.key.analyze_mode", GLFW.GLFW_KEY_LEFT_CONTROL, true)
+    private val openContentBook = registerKey("wynnlib.key.content_book", GLFW.GLFW_KEY_GRAVE_ACCENT)
+
     fun init() {
         ClientTickEvents.END_CLIENT_TICK.register(ClientTickEvents.EndTick {
             while (openHandbook.wasPressed()) {
                 it.setScreen(EquipmentDictScreen(it.currentScreen))
+            }
+            while (openContentBook.wasPressed()) {
+                it.player?.currentScreenHandler?.syncId?.let { syncId -> it.interactionManager?.clickSlot(syncId, 43, 1, SlotActionType.PICKUP, it.player) }
             }
         })
     }
